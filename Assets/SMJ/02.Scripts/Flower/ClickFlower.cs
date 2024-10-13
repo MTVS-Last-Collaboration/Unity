467568249b158a5b0de5f8c5b36472285aae5444
SongMinJe <dlsgur3543@gmail.com>
2024-10-14 01:34:59 +0900

    SMJ_꽃 UI 예외처리
    1. 타 플레이어를 위한 예외처리 추가

--- Assets/SMJ/02.Scripts/Flower/ClickFlower.cs
+++ Assets/SMJ/02.Scripts/Flower/ClickFlower.cs
@@ -7,6 +7,11 @@ public class ClickFlower : MonoBehaviour
 {
     private Flower targetFlower;
     private bool isPlayerInRange = false;
+    private CheckID checkID;
+
+    [SerializeField] private string talkText;
+    [SerializeField] private string resultText;
+    [SerializeField] private string voiceNullText;
 
     private void Start()
     {
@@ -15,9 +20,36 @@ public class ClickFlower : MonoBehaviour
 
     private void OnMouseDown()
     {
+        //추후 클릭 성공 시 플레이어 움직임 막기
         if (isPlayerInRange && targetFlower != null && targetFlower.uiManager != null)
         {
-            targetFlower.uiManager.ShowFlowerInfo(targetFlower);
+            if (checkID != null)
+            {
+                if (checkID.IsMine(targetFlower) == true)
+                {
+                    targetFlower.uiManager.ShowFlowerInfo(targetFlower, 0);
+                    //연인에게 따뜻한 한마디 말하기
+                    //녹음 완료시 + "(완료)" 추가 및 버튼액션 비활성화
+                    targetFlower.uiManager.UpdateButtonText(talkText);
+                    targetFlower.uiManager.UpdateButtonInteractable(true);
+                }
+                else
+                {
+                    targetFlower.uiManager.ShowFlowerInfo(targetFlower, 1);
+                    //아직 따뜻하지 않아요...
+                    //연인의 말한마디 듣기
+                    if (targetFlower.voiceClip == null) 
+                    {
+                        targetFlower.uiManager.UpdateButtonText(voiceNullText);
+                        targetFlower.uiManager.UpdateButtonInteractable(false);
+                    }
+                    else
+                    {
+                        targetFlower.uiManager.UpdateButtonText(resultText);
+                        targetFlower.uiManager.UpdateButtonInteractable(true);
+                    }
+                }
+            }
         }
     }
 
@@ -26,6 +58,7 @@ public class ClickFlower : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             isPlayerInRange = true;
+            checkID = other.GetComponent<CheckID>();
         }
     }
 
@@ -34,10 +67,12 @@ public class ClickFlower : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             isPlayerInRange = false;
-            if (targetFlower != null && targetFlower.uiManager != null)
-            {
-                targetFlower.uiManager.HideFlowerInfo();
-            }
+            //if (targetFlower != null && targetFlower.uiManager != null)
+            //{
+            //    targetFlower.uiManager.HideFlowerInfo();
+            //    checkID = null;
+            //}
+            //추후 끄는 버튼 생성
         }
     }
 }
--- Assets/SMJ/02.Scripts/Flower/FlowerUIManager.cs
+++ Assets/SMJ/02.Scripts/Flower/FlowerUIManager.cs
@@ -6,30 +6,44 @@ using UnityEngine.UI;
 
 public class FlowerUIManager : MonoBehaviour
 {
-    [SerializeField] private GameObject uiPanel;
+    [SerializeField] private GameObject[] uiPanel;
     [SerializeField] private TMP_Text nameText;
+    [SerializeField] private TMP_Text buttonText;
+    [SerializeField] private Button btn;
     [SerializeField] private Image flowerImg;
 
-    private void Awake() => HideFlowerInfo();
-
-    public void ShowFlowerInfo(Flower flower)
+    public void ShowFlowerInfo(Flower flower, int idx)
     {
         if (flower == null)
         {
-            Debug.LogError("Attempted to show info for null flower");
             return;
         }
-        UpdateUI(flower);
-        uiPanel.SetActive(true);
+        //UpdateUI(flower);
+        uiPanel[idx].SetActive(true);
     }
 
-    public void HideFlowerInfo() => uiPanel.SetActive(false);
-
-    private void UpdateUI(Flower flower)
+    public void HideFlowerInfo(int idx)
     {
-        nameText.text = flower.nickName;
+        uiPanel[idx].SetActive(false);
+    }
+
+    public void UpdateUI(Flower flower)
+    {
+        //nameText.text = flower.nickName;
         // 추가적인 UI 업데이트 로직
     }
 
-    public void OnCloseButtonClick() => HideFlowerInfo();
+    public void UpdateButtonText(string text)
+    {
+        buttonText.text = text;
+    }
+    public void UpdateButtonInteractable(bool isInteractable)
+    {
+        btn.interactable = isInteractable;
+    }
+
+    public void OnCloseButtonClick(int idx)
+    {
+        HideFlowerInfo(idx);
+    }
 }
