2cb7a31a316bbb616fbaa21b1f029b537278ee67
SEOKWON <50188612+seokwonJ@users.noreply.github.com>
2024-10-16 18:34:06 +0900

    add chatting,
    반려몽 채팅 혼자서 하는 거라도 구현
    방꾸미기 대강 구현

---
+++ Assets/JSW/Scripts/DecoRoom/JSW_DecorateRoomManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JSW_DecorateRoomManager : MonoBehaviour
+{
+    public bool [,] roomPosition = new bool [10, 10];
+
+    public enum funiture
+    {
+        Chair,
+        Table,
+        Bed
+    }
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+    // 여기서 필요한 것
+    // 플레이어 위치 받기
+    // 가구들 배치 받기
+
+
+    // 플레이어가 필요한것
+    // 물건 잡을 때 상호작용키 f
+    // 물건 밀거나 당길 때 움직일 수 있는 장소인지를 매니저 배열에서 조사
+    // 밀때 같이 이동하기
+    // 잡을 때 1,1 정수 위치로 이동하기
+    // 밀려고 할 때 다른 플레이어 위치 받아오기
+    // 당길 때도 플레이어 위치 받아오기
+    // 물건 꺼내기 꺼낼 때 주변 놓을 수 있을지 확인하기
+
+    
+
+}
---
+++ Assets/JSW/Scripts/JSW_ScheduleManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class JSW_ScheduleManager : MonoBehaviour
+{
+    // 추후 아이콘도 추가할 예정
+
+
+    public TMP_InputField input_Field;
+    public RectTransform trcontent;
+    public GameObject scheduleFactory;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        input_Field = GameObject.Find("Schedule_Input").GetComponent<TMP_InputField>();
+        trcontent = GameObject.Find("ScheduleContentBody").GetComponent<RectTransform>();
+    }
+
+
+
+    public void ScheduleSubmit()
+    {
+
+        string chat = input_Field.text;
+
+        CreateScheduleItem(chat, Color.black);
+    }
+
+    void CreateScheduleItem(string chat, Color chatColor)
+    {
+        // s의 내용으로 ChatItem을 만들자.
+        GameObject go = Instantiate(scheduleFactory, trcontent);
+        // 만들어진 go에서 ChatItem 컴포넌트 가져오자.
+        JSW_ScheduleItem scheduleItem = go.GetComponent<JSW_ScheduleItem>();
+
+        // 가져온 컴포넌트의 SetText 함수 실행
+        scheduleItem.SetText(chat);
+    }
+}
