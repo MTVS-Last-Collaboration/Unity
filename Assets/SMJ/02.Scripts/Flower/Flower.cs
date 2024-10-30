13a0968f73afb8ed21f309b824f3100f8b58924e
SongMinJe <dlsgur3543@gmail.com>
2024-10-30 18:31:30 +0900

    SMJ_백엔드 사전작업_1

--- Assets/SMJ/02.Scripts/Flower/Flower.cs
+++ Assets/SMJ/02.Scripts/Flower/Flower.cs
@@ -1,13 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+[Serializable]
+public class FlowerData
+{
+    public string managerId;
+    public string nickName;
+    public byte[] voiceData;
+    public int evolutionCount;
+    public int harvestCoins;
+    public string state;
+}
 
 public class Flower : MonoBehaviour
 {
     public string managerId;
     public string nickName;
     public AudioClip voiceClip;
-    public bool isTouchAble = true;
+    //public bool isTouchAble = true;   // 꽃 터치시 조이스틱 등 비활성화 > ui끄면 다시 활성화
     public FlowerUIManager uiManager;
     public int evolutionCount = 0;
     public int harvestCoins = 300;
@@ -27,8 +39,8 @@ public class Flower : MonoBehaviour
 
     public void ResetFlower()
     {
-        managerId = string.Empty;
-        nickName = string.Empty;
+        //managerId = string.Empty;
+        //nickName = string.Empty;
         voiceClip = null;
         evolutionCount = 0;
         curState = States.SPROUT;
