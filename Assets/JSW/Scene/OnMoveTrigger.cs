46bafe28bf1f64cdc2847905de690c65004c7b01
SEOKWON <50188612+seokwonJ@users.noreply.github.com>
2024-11-14 17:59:56 +0900

    add Hoon UI

--- Assets/JSW/Scene/OnMoveTrigger.cs
+++ Assets/JSW/Scene/OnMoveTrigger.cs
@@ -12,17 +12,7 @@ public class OnMoveTrigger : MonoBehaviourPunCallbacks
     public GameObject byeUI;
     public JSW_SoundManager soundManager;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnTriggerEnter(Collider other)
     {
